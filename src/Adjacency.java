b773d9c15e71ea9323893d02e181924432947bdf
MokiMeki <snow_hotel@hotmail.com>
2015-09-11 17:56:16 +0700

    Change file path Change Csv value

--- src/Adjacency.java
+++ src/Adjacency.java
@@ -7,8 +7,8 @@ public class Adjacency {
     public Adjacency()
     {
         table = new int[SIZE][SIZE];
-        for(int i=0;i<20;i++) {
-            for(int j=0;j<20;j++) {
+        for(int i=0;i<SIZE;i++) {
+            for(int j=0;j<SIZE;j++) {
                 table[i][j] = -1;
             }
         }
