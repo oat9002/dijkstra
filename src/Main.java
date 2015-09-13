ecfe887e9d67b2a521a1024fa3f4cb9786c51b35
MokiMeki <snow_hotel@hotmail.com>
2015-09-13 23:52:24 +0700

    Add some method in Adjacency class , init vertex and adjacency in main class , prompt for input start&end node from user

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 /**
  * Created by oat90 on 9/9/2558.
  */
@@ -5,10 +9,16 @@ public class Main {
 
     public static void main(String args[])
     {
-
         CSVFileReader csv = new CSVFileReader("test.csv");
         String[][] table = csv.getFileValue();
+        Adjacency adjacency;
+        Vertex[] vertexList;
+        Scanner sc = new Scanner(System.in);
+        char startVertex;
+        char endVertex;
 
+
+        //print array
         for(int i=0;i<table.length;i++)
         {
             for(int j=0;j<table[i].length;j++)
@@ -17,7 +27,29 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println();
 
-        
+        vertexList = new Vertex[table.length];
+        adjacency = new Adjacency(table.length);
+
+        //init vertex
+        for(int i=0;i<table[0].length;i++)
+        {
+            vertexList[i] = new Vertex(table[0][i].charAt(0));
+        }
+
+        //init adjacency
+        for(int i=1;i<table.length;i++)
+        {
+            for(int j=0;j<table[i].length;j++)
+            {
+                adjacency.setWeight(i,j,Integer.parseInt(table[i][j]));
+            }
+        }
+
+        System.out.print("Enter Start node name : ");
+        startVertex = sc.next().charAt(0);
+        System.out.print("Enter End node name : ");
+        endVertex = sc.next().charAt(0);
     }
 }
